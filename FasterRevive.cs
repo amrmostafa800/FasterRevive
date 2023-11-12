814996bf4720f5a21dd58d77c749a063345cbbe5
amr <amrmostafa800@gmail.com>
2023-11-12 02:43:09 +0200

    Add project files.

---
+++ FasterRevive.cs
@@ -0,0 +1,20 @@
+﻿using BepInEx;
+using HarmonyLib;
+
+namespace FasterRevive
+{
+    [BepInPlugin(GUID, NAME, VERSION)]
+    public class FasterRevive : BaseUnityPlugin
+    {
+        public const string
+            GUID = "FasterRevive",
+            NAME = "FasterRevive",
+            VERSION = "1.0.0";
+
+        public void Awake()
+        {
+            Harmony.CreateAndPatchAll(typeof(MuckPatch));
+            Logger.LogMessage("Loaded FasterRevive");
+        }
+    }
+}
---
+++ Patches.cs
@@ -0,0 +1,20 @@
+﻿using HarmonyLib;
+
+namespace FasterRevive
+{
+    public class MuckPatch
+    {
+        //[HarmonyPrefix, HarmonyPatch(typeof(GraveInteract), "set_timeLeft")]
+        //public static bool set_timeLeft(float value)
+        //{
+        //    return false;
+        //}
+
+        [HarmonyPrefix, HarmonyPatch(typeof(GraveInteract), nameof(GraveInteract.SetTime))]
+        public static bool SetTime(ref float time)
+        {
+            time = 10f;
+            return true;
+        }
+    }
+}
